f8192d554902e5c691237a84f9b353023d99e208
HassanAlkuheli <halkhyly@gmail.com>
2026-02-10 21:28:52 +0300

    fix(deploy): unsuppress migration errors and simplify migration 0008

--- backend/drizzle/0008_luxuriant_wild_pack.sql
+++ backend/drizzle/0008_luxuriant_wild_pack.sql
@@ -1,4 +1,3 @@
 CREATE TYPE "public"."project_type" AS ENUM('static', 'nodejs');--> statement-breakpoint
-ALTER TYPE "public"."project_status" ADD VALUE 'STARTING' BEFORE 'RUNNING';--> statement-breakpoint
 ALTER TABLE "projects" ADD COLUMN "admin_url" text;--> statement-breakpoint
 ALTER TABLE "projects" ADD COLUMN "project_type" "project_type" DEFAULT 'static' NOT NULL;
